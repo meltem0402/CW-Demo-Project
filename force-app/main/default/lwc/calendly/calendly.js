951a4b3e7f89561c461950f72315cbe477235abc
Meltem <meltemvurgun@gmail.com>
2022-11-22 20:28:06 -0500

    calendly

--- force-app/main/default/lwc/calendly/calendly.js
+++ force-app/main/default/lwc/calendly/calendly.js
@@ -1,6 +1,7 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api } from 'lwc';
 
-export default class Calendly extends LightningElement {
+export default class CalendlyLWC extends LightningElement {
+
     @api calendlyURL;
 
     connectedCallback(){
